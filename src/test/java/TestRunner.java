a004dbd228773556294e09b95e3a9dd5a6da4d69
Damir <vip.ushkempir@mail.ru>
2025-09-20 20:19:57 +0500

    init: maven, junit5, readme

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestRunner {
+    @Test
+    void appStart() {
+        assertDoesNotThrow(() -> App.main(new String[]{}));
+    }
+}
